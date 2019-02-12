faa3968ba1f39891546f4d211f080bbb2fb4a47c
Hasnatul Jelan <43754044+HasnatPranto@users.noreply.github.com>
2019-02-12 21:08:15 +0600

    Update SimplePDFConversion.cpp

--- Source Codes/SimplePDFConversion.cpp
+++ Source Codes/SimplePDFConversion.cpp
@@ -1,1 +1,41 @@
+#include<iostream>
 
+#include<fstream>
+
+using namespace std;
+
+int main (void)
+{
+	ifstream iFile;
+	ofstream oFile;
+	
+	string s;
+	
+	iFile.open("/home/pranto/Downloads/sample.pdf");
+	oFile.open("/home/pranto/Desktop/t.txt");
+	
+	if(oFile.is_open() && iFile.is_open())
+	{
+		iFile >> s;
+		
+		while(!iFile.eof())
+		{
+			if(s=="(")
+			{
+				iFile >> s;
+				
+				while(s!=")"){
+					oFile << s <<' ';
+					iFile >> s;
+				}	 
+			}
+			iFile >> s;
+		}
+	  } 
+	    
+	
+	iFile.close(); 
+	oFile.close();
+	
+	return 0;	
+}	
